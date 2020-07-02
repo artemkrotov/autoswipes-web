1b1812935cb2999074dbf231fc1cd1bd0caa34ac
Nikita <55nkdev@gmail.com>
2020-07-02 20:56:15 +0600

    rewrite auth base url

--- src/constants.js
+++ src/constants.js
@@ -4,6 +4,6 @@ export const ACCESS_TOKEN = 'token';
 
 export const OAUTH2_REDIRECT_URI = 'https://web.autoswipes.com/oauth2/redirect'
 
-export const FACEBOOK_AUTH_URL = API_BASE_URL + '/oauth2/authorize/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
-export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
+export const FACEBOOK_AUTH_URL = AUTH_BASE_URL + '/oauth2/authorize/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
+export const GOOGLE_AUTH_URL = AUTH_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
 export const VK_AUTH_URL = '/';
