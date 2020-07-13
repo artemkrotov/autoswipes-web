ed14b7977445a6504cf9680d4a36271e6f31a754
Nikita Kravchenko <55nkdev@gmail.com>
2020-07-14 01:15:11 +0600

    add enot and comment faq

--- src/components/Rates/rates.js
+++ src/components/Rates/rates.js
@@ -69,20 +69,20 @@ function LegacyRates () {
                 </div>
             </div>
 
-            <div className="rates-questions" style={{"display": "none"}}>
-                <div className="rates-questions__item">
-                    <div className="rates-questions__header">Lorem ipsum dolor sit amet, consectetur adipisicing elit? </div>
-                    <div className="rates-questions__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in</div>
-                </div>
-                <div className="rates-questions__item">
-                    <div className="rates-questions__header">Lorem ipsum dolor sit amet, consectetur adipisicing elit? </div>
-                    <div className="rates-questions__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in</div>
-                </div>
-                <div className="rates-questions__item">
-                    <div className="rates-questions__header">Lorem ipsum dolor sit amet, consectetur adipisicing elit? </div>
-                    <div className="rates-questions__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-                </div>
-            </div>
+            {/*<div className="rates-questions" style={{"display": "none"}}>*/}
+            {/*    <div className="rates-questions__item">*/}
+            {/*        <div className="rates-questions__header">Lorem ipsum dolor sit amet, consectetur adipisicing elit? </div>*/}
+            {/*        <div className="rates-questions__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in</div>*/}
+            {/*    </div>*/}
+            {/*    <div className="rates-questions__item">*/}
+            {/*        <div className="rates-questions__header">Lorem ipsum dolor sit amet, consectetur adipisicing elit? </div>*/}
+            {/*        <div className="rates-questions__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in</div>*/}
+            {/*    </div>*/}
+            {/*    <div className="rates-questions__item">*/}
+            {/*        <div className="rates-questions__header">Lorem ipsum dolor sit amet, consectetur adipisicing elit? </div>*/}
+            {/*        <div className="rates-questions__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
         </div>
     );
 };
