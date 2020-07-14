b082572c0f15070b4a4a04235f1fc903e373b303
Nikita Kravchenko <55nkdev@gmail.com>
2020-07-14 17:24:03 +0600

    Revert "Update oauth urls"
    This reverts commit 0a4283944d474954d1de431cb3ca4afbff93d6ae.

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,9 @@
+export const AUTH_BASE_URL = 'https://auth.autoswipes.com';
 export const API_BASE_URL = 'https://api.autoswipes.com';
 export const ACCESS_TOKEN = 'token';
 
 export const OAUTH2_REDIRECT_URI = 'https://web.autoswipes.com/oauth2/redirect'
 
-export const FACEBOOK_AUTH_URL = API_BASE_URL + '/auth-service/oauth2/authorize/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
-export const GOOGLE_AUTH_URL = API_BASE_URL + '/auth-service/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
+export const FACEBOOK_AUTH_URL = AUTH_BASE_URL + '/oauth2/authorize/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
+export const GOOGLE_AUTH_URL = AUTH_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
 export const VK_AUTH_URL = '/';
