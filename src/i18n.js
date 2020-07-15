5aaf16fabae5bf6a6ce00714a171f20d5cd7a314
Nikita Kravchenko <55nkdev@gmail.com>
2020-07-16 01:09:11 +0600

    mb fix locale

--- src/i18n.js
+++ src/i18n.js
@@ -8,7 +8,7 @@ i18n
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
-        fallbackLng: 'ru',
+        fallbackLng: 'ru-RU',
         // debug: true,
 
         interpolation: {
